e90aa538bcc7fceed600c63ecb4786e4bdb8fa2e
EnricoCiccolini <piroroma90@gmail.com>
2025-12-17 10:45:48 +0100

    feat: Add react-snowfall dependency for enhanced visual effects

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,12 @@
 
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
-
+import Snowfall from "react-snowfall"
 
 function MainLayout() {
 
     return <>
-
+        <Snowfall color="white" />
 
         <Header />
         <main className="container flex-grow-1">
